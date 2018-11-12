4b45f49e04b28b89c4929ac8f8a5db74cfa21699
Vincent Woo <realplord@gmail.com>
2018-11-12 20:44:08 +1300

    Downgrade Kotlin version due to corutines conflict.

---
+++ app/src/main/java/com/vincent/pokeapi/model/PokemonViewModel.kt
@@ -0,0 +1,21 @@
+package com.vincent.pokeapi.model
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.vincent.entities.Pokemon
+import com.vincent.usecases.GetPokemons
+import com.vincent.usecases.service.IPokeService
+import kotlinx.coroutines.experimental.async
+
+class PokemonViewModel(private val pokeService: IPokeService) : ViewModel() {
+    val pokemons: MutableLiveData<List<Pokemon>> = MutableLiveData()
+
+    suspend fun getPokemons() {
+        val searchWork = async { GetPokemons(pokeService).execute() }
+        val result = searchWork.await()
+
+        result?.let {
+            pokemons.postValue(result)
+        }
+    }
+}
